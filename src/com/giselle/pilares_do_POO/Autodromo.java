c4f753ffeaaae0f6567bdf5566cfdfe70dc0ceb4
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-22 19:57:03 -0300

    criada classe carro -  Pilares POO

---
+++ src/com/giselle/pilares_do_POO/Autodromo.java
@@ -0,0 +1,11 @@
+package com.giselle.pilares_do_POO;
+
+public class Autodromo {
+  public static void main(String[] args) {
+
+    Carro jeep = new Carro();
+    jeep.checkCalibragemPneus();
+    jeep.checkCombustivelDisponivel();
+    jeep.ligar();
+  }
+}
---
+++ src/com/giselle/pilares_do_POO/Carro.java
@@ -0,0 +1,16 @@
+package com.giselle.pilares_do_POO;
+
+public class Carro {
+
+  public void ligar() {
+    System.out.println("O carro está ligado");
+  }
+
+  public void checkCombustivelDisponivel() {
+    System.out.println("Verificando combustivel");
+  }
+
+  public void checkCalibragemPneus() {
+    System.out.println("Verificando se pneus estão calibrados");
+  }
+}
