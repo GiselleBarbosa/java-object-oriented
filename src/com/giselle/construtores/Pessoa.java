77cc6d65aa107104b95ceb432aaa56dc86d9b6c1
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-22 19:01:23 -0300

    adicionado construtor

--- src/com/giselle/construtores/Pessoa.java
+++ src/com/giselle/construtores/Pessoa.java
@@ -4,19 +4,27 @@ public class Pessoa {
 	private String nome;
 	private String cpf;
 	private String endereco;
-	
+
+	public Pessoa(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
+
+	public void setEndereco(String newEndereco) {
+		endereco = newEndereco;
 	}
-	//...
-	//setters de nome e cpf ?
+	// ...
+	// setters de nome e cpf ?
 }
--- src/com/giselle/construtores/SistemaCadastro.java
+++ src/com/giselle/construtores/SistemaCadastro.java
@@ -2,16 +2,16 @@ package com.giselle.construtores;
 
 public class SistemaCadastro {
 	public static void main(String[] args) {
-		//criamos uma pessoa no sistema
-		Pessoa marcos = new Pessoa();
-		
-		//definimos o endereço de marcos
+		// criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("123", "MARCOS SILVA");
+
+		// definimos o endereço de marcos
 		marcos.setEndereco("RUA DAS MARIAS");
-		
-		//e como definir o nome e cpf do marcos ?
-		
-		//imprimindo o marcos sem o nome e cpf
-		
+
+		// e como definir o nome e cpf do marcos ?
+
+		// imprimindo o marcos sem o nome e cpf
+
 		System.out.println(marcos.getNome() + "-" + marcos.getCpf());
 	}
 }
